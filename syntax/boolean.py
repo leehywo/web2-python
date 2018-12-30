004003d1272ccf1fc51e1d4af0775c801903cfa7
leehywo <leehywo@gmail.com>
2018-12-30 20:56:28 +0900

    boolean

---
+++ syntax/boolean.py
@@ -0,0 +1,24 @@
+#Number
+print(1) #Integer
+
+#String
+print("Hello world")
+
+#Boolean
+print(True) #참
+print(False) #거짓
+
+#Expressions
+print(1+1)
+print("Hello " + "World")
+
+#Comparison Operator
+print(1==1)
+print(1<2)
+print(1>2)
+
+#Membership Operator
+print('world' in 'Hello world')
+
+import os
+print(os.path.exists('boolean2.py'))
