6479335d1c787845e96a08f697f43bf1809ae9ca
leehywo <leehywo@gmail.com>
2018-12-30 19:53:23 +0900

    first commit

---
+++ syntax/string.py
@@ -0,0 +1,24 @@
+print("Hello world")
+print('Hello world')
+
+print("Hell'o' world")
+
+#escape
+print("Hell\'o\' \"w\"orld")
+
+#newline
+print("H")
+print("e")
+print("l")
+print("l")
+print("o")
+print("\nH\ne\nl\nl\no")
+
+#docString
+print('''
+H 
+e 
+l
+l
+o
+''')
