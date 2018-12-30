3ee984587a05001d195cfb4ef5922627c947f39f
leehywo <leehywo@gmail.com>
2018-12-30 21:25:51 +0900

    ~ep.24 Conditional

---
+++ syntax/conditional.py
@@ -0,0 +1,23 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+'''
+if user_id == 'leehywo':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else :
+    print('Who are you?')
+'''
+
+if user_id == 'leehywo' and user_pwd == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
